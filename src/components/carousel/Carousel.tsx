ea0c18ccee89223151f407ed56cd8c756fd81203
Vivek Singh <viveksinghup.singh@gmail.com>
2023-09-13 00:44:47 +0530

    changes

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -46,75 +46,78 @@ const Carousel = ({ data, loading, endpoint, title }) => {
 
     return (
         <div className="carousel">
-            <ContentWrapper>
-                {title && <div className="carouselTitle">{title}</div>}
-                <BsFillArrowLeftCircleFill
-                    className="carouselLeftNav arrow"
-                    onClick={() => navigation("left")}
-                />
-                <BsFillArrowRightCircleFill
-                    className="carouselRighttNav arrow"
-                    onClick={() => navigation("right")}
-                />
-                {!loading ? (
-                    <div className="carouselItems" ref={carouselContainer}>
-                        {data?.map((item) => {
-                            const posterUrl = item.poster_path
-                                ? url.poster + item.poster_path
-                                : PosterFallback;
-                            return (
-                                <div
-                                    key={item.id}
-                                    className="carouselItem"
-                                    onClick={() =>
-                                        navigate(
-                                            `/${item.media_type || endpoint}/${
-                                                item.id
-                                            }`
-                                        )
-                                    }
-                                >
-                                    <div className="posterBlock">
-                                        <Img src={posterUrl} />
-                                        <CircleRating
-                                            rating={item.vote_average.toFixed(
-                                                1
-                                            )}
-                                        />
-                                        <div className="cardText">
-                                            <Genres
-                                                data={item.genre_ids.slice(
-                                                    0,
-                                                    2
+            {data?.length > 0 && (
+                <ContentWrapper>
+                    {title && <div className="carouselTitle">{title}</div>}
+                    <BsFillArrowLeftCircleFill
+                        className="carouselLeftNav arrow"
+                        onClick={() => navigation("left")}
+                    />
+                    <BsFillArrowRightCircleFill
+                        className="carouselRighttNav arrow"
+                        onClick={() => navigation("right")}
+                    />
+                    {!loading ? (
+                        <div className="carouselItems" ref={carouselContainer}>
+                            {data?.map((item) => {
+                                const posterUrl = item.poster_path
+                                    ? url.poster + item.poster_path
+                                    : PosterFallback;
+                                return (
+                                    <div
+                                        key={item.id}
+                                        className="carouselItem"
+                                        onClick={() =>
+                                            navigate(
+                                                `/${
+                                                    item.media_type || endpoint
+                                                }/${item.id}`
+                                            )
+                                        }
+                                    >
+                                        <div className="posterBlock">
+                                            <Img src={posterUrl} />
+                                            <CircleRating
+                                                rating={item.vote_average.toFixed(
+                                                    1
                                                 )}
                                             />
-                                            <div className="textBlock">
-                                                <span className="title">
-                                                    {item.title || item.name}
-                                                </span>
-                                                <span className="date">
-                                                    {dayjs(
-                                                        item.release_date ||
-                                                            item.first_air_date
-                                                    ).format("MMM D, YYYY")}
-                                                </span>
+                                            <div className="cardText">
+                                                <Genres
+                                                    data={item.genre_ids.slice(
+                                                        0,
+                                                        2
+                                                    )}
+                                                />
+                                                <div className="textBlock">
+                                                    <span className="title">
+                                                        {item.title ||
+                                                            item.name}
+                                                    </span>
+                                                    <span className="date">
+                                                        {dayjs(
+                                                            item.release_date ||
+                                                                item.first_air_date
+                                                        ).format("MMM D, YYYY")}
+                                                    </span>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            );
-                        })}
-                    </div>
-                ) : (
-                    <div className="loadingSkeleton">
-                        {skItem()}
-                        {skItem()}
-                        {skItem()}
-                        {skItem()}
-                        {skItem()}
-                    </div>
-                )}
-            </ContentWrapper>
+                                );
+                            })}
+                        </div>
+                    ) : (
+                        <div className="loadingSkeleton">
+                            {skItem()}
+                            {skItem()}
+                            {skItem()}
+                            {skItem()}
+                            {skItem()}
+                        </div>
+                    )}
+                </ContentWrapper>
+            )}
         </div>
     );
 };
